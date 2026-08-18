edc981df8e2f7a26ac63ae5a34cc5d4a276207a1
Igor Sicorschi <igor.sicorschi@gmail.com>
2026-08-18 19:19:29 +0200

    add: device module

---
+++ src/devices/devices.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { CreateDeviceDto } from './dto/create-device.dto';
+import { UpdateDeviceDto } from './dto/update-device.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Device } from './entities/device.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class DevicesService {
+  constructor(
+    @InjectRepository(Device)
+    private readonly deviceRepository: Repository<Device>,
+  ) {}
+
+  create(createDeviceDto: CreateDeviceDto) {
+    const device = this.deviceRepository.create(createDeviceDto);
+    return this.deviceRepository.save(device);
+  }
+
+  findAll() {
+    return this.deviceRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.deviceRepository.findOneBy({ id });
+  }
+
+  async update(id: number, updateDeviceDto: UpdateDeviceDto) {
+    await this.deviceRepository.update(id, updateDeviceDto);
+    return this.deviceRepository.findOneBy({ id });
+  }
+
+  async remove(id: number) {
+    await this.deviceRepository.delete(id);
+    return { deleted: true };
+  }
+}
---
+++ src/devices/entities/device.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { TimestampedEntity } from '../../auth/entities/timestamp.entity';
+
+@Entity('device')
+export class Device extends TimestampedEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  type!: string;
+
+  @Column({ nullable: true, default: 'offline' })
+  status!: string;
+
+  @Column({ nullable: true, default: 'unknown' })
+  location!: string;
+
+  @Column()
+  ip!: string;
+
+  @Column({ nullable: true, default: 'unknown' })
+  uptime!: string;
+
+  @Column({ nullable: true, default: 'unknown' })
+  temperature!: string;
+
+  @Column({ nullable: true, default: 'unknown' })
+  version!: string;
+
+  @Column({ nullable: true, default: 'unknown' })
+  memory!: string;
+}
