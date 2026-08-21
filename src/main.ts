d50f971e9f452a1e4d4c38a114c2dbc62f52354d
Igor Sicorschi <igor.sicorschi@gmail.com>
2026-08-21 02:44:12 +0200

    dockerize the api

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import type { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,15 +18,29 @@ async function bootstrap() {
     }),
   );
   const corsOptions = {
-    origin: 'http://localhost:4200',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   };
 
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+      );
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
   app.enableCors(corsOptions);
   const port = Number(process.env.API_PORT ?? 3000);
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   Logger.log(`Application is running on: http://localhost:${port}`);
   Logger.log('DB connected successfully');
 }
